fe5ae773f154aa9745bfe130933560c1e2419668
Petro Kirushkin <pkyriushkin@nymbus.com>
2022-07-29 22:24:45 +0300

    add import for british-to-american-titles module

--- components/translator.js
+++ components/translator.js
@@ -1,6 +1,7 @@
 const americanOnly = require("./american-only.js");
 const americanToBritishSpelling = require("./american-to-british-spelling.js");
 const americanToBritishTitles = require("./american-to-british-titles.js");
+const britishToAmericanTitles = require("./british-to-american-titles.js");
 const britishOnly = require("./british-only.js");
 const Constants = require("../constans.js");
 
